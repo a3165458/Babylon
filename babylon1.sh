1eaa9a4d10a3192cc63d63e0558610cee1922f90
smeb y <48400087+a3165458@users.noreply.github.com>
2024-02-22 18:28:18 +0800

    Update babylon1.sh

--- babylon1.sh
+++ babylon1.sh
@@ -38,8 +38,8 @@ sudo apt update && sudo apt upgrade -y
 sudo apt -qy install curl git jq lz4 build-essential
 
 # Install GO
-rm -rf /usr/local/go
-curl -Ls https://go.dev/dl/go1.21.6.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+curl -Ls https://go.dev/dl/go1.21.6.linux-amd64.tar.gz
+ rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go version
 
