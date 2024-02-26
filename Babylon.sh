f3b9e28fecb5c1da903e58ea200acebed5c8f62a
smeb y <48400087+a3165458@users.noreply.github.com>
2024-02-26 10:55:58 +0800

    Update Babylon.sh

--- Babylon.sh
+++ Babylon.sh
@@ -39,8 +39,8 @@ sudo apt -qy install curl git jq lz4 build-essential
 # 安装 Go
 sudo rm -rf /usr/local/go
 curl -L https://go.dev/dl/go1.21.6.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
-echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
-source .bash_profile
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.profile
+source $HOME/.profile
 mkdir -p $HOME/go/bin
 echo "export GOPATH=$HOME/go" >> ~/.bashrc
 source ~/.bashrc
