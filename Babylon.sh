2a258acbd55ca390da8a3cb9e843708c1c0f9439
smeb y <48400087+a3165458@users.noreply.github.com>
2024-03-12 17:53:27 +0800

    Update Babylon.sh

--- Babylon.sh
+++ Babylon.sh
@@ -37,13 +37,18 @@ sudo apt update && sudo apt upgrade -y
 sudo apt -qy install curl git jq lz4 build-essential
 
 # 安装 Go
-cd $HOME
-ver="1.22.0"
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+rm -rf $HOME/go
 sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
-source $HOME/.bash_profile
+cd $HOME
+curl https://dl.google.com/go/go1.22.0.linux-amd64.tar.gz | sudo tar -C/usr/local -zxvf -
+cat <<'EOF' >>$HOME/.profile
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export GO111MODULE=on
+export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+EOF
+source $HOME/.profile
+go version
 
 
 # 克隆项目仓库
