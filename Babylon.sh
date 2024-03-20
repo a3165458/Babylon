4ec5539e9e9a35b6932f60cde6fd9db8b2be6d46
smeb y <48400087+a3165458@users.noreply.github.com>
2024-03-20 16:19:03 +0800

    Update Babylon.sh

--- Babylon.sh
+++ Babylon.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# 检查是否以root用户运行脚本
+if [ "$(id -u)" != "0" ]; then
+    echo "此脚本需要以root用户权限运行。"
+    echo "请尝试使用 'sudo -i' 命令切换到root用户，然后再次运行此脚本。"
+    exit 1
+fi
+
 # 脚本保存路径
 SCRIPT_PATH="$HOME/Babylon.sh"
 
