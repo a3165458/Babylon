30681f0c208e1dcdd8fa0470c4414cbaf44bbaaf
smeb y <48400087+a3165458@users.noreply.github.com>
2024-04-01 17:17:10 +0800

    Update Babylon.sh

--- Babylon.sh
+++ Babylon.sh
@@ -181,6 +181,11 @@ function check_sync_status() {
 function main_menu() {
     while true; do
         clear
+        echo "脚本以及教程由推特用户大赌哥 @y95277777 编写，免费开源，请勿相信收费"
+        echo "================================================================"
+        echo "节点社区 Telegram 群组:https://t.me/niuwuriji"
+        echo "节点社区 Telegram 频道:https://t.me/niuwuriji"
+        echo "退出脚本，请按键盘ctrl c退出即可"
         echo "请选择要执行的操作:"
         echo "1. 安装节点"
         echo "2. 创建钱包"
